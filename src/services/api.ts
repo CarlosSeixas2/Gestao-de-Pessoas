77a30599d3f5c1e2b639d7e0fd376a5781c7fdd2
Seixas <carlosseixasof@gmail.com>
2025-06-16 11:08:48 -0300

    Adicionando variavel de ambiente no service API

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,7 @@
 import axios from "axios";
 
+const API_URL = process.env.REACT_APP_API_URL || "http://localhost:3001";
+
 export const api = axios.create({
-  baseURL: "http://localhost:3001",
+  baseURL: API_URL,
 });
