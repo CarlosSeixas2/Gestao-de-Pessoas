8daf872de362d1ea98b614632cf8640e1bbaea29
Seixas <carlosseixasof@gmail.com>
2025-06-16 11:40:09 -0300

    Correcao Importacao API

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const API_URL = process.env.REACT_APP_API_URL || "http://localhost:3001";
+const API_URL = import.meta.env.VITE_API_URL || "http://localhost:3001";
+
+// console.log("API URL:", API_URL);
 
 export const api = axios.create({
   baseURL: API_URL,
